1c93a84d749fd5c94ee54001b32f2907191cd9cb
AnastasiiaK120 <anastasiiakudriavtseva4@gmail.com>
2021-09-03 22:33:47 -0400

    Testing the code

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Post
+
+class BlogTest(TestCase):
+    def setUp(self) -> None:
+
+        test = User.objects.create(username="test", password="test123")
+        test.save()
+
+        post = Post.objects.create(author=test, title='Hello test', body="Body test")
+        post.save()
+
+    def test_post_content(self):
+        post = Post.objects.get(id=1)
+        author_bd = post.author
+
+        self.assertEqual(str(author_bd), "test")

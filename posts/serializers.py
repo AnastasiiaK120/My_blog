821e468225362c31d4b33eb2ad6bd725ef5b8be0
AnastasiiaK120 <anastasiiakudriavtseva4@gmail.com>
2021-09-04 16:31:09 -0400

    Add router.register

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'author', 'title', 'body', 'create')
         model = Post
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', 'username')
+        model = get_user_model()
--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,22 @@
-from django.urls import path, include
-from .views import PostList, PostDetaiil
+# from django.urls import path, include
+# from .views import (PostList, PostDetaiil, UserDetail, UserList,)
+#
 
-urlpatterns = [
-    path('<int:pk>', PostDetaiil.as_view()),
-    path('', PostList.as_view()),
-]
+
+# urlpatterns = [
+#     path('users/', UserList.as_view()),
+#     path('users/<int:pk>/', UserDetail.as_view()),
+#
+#     path('<int:pk>', PostDetaiil.as_view()),
+#     path('', PostList.as_view()),
+# ]
+
+from rest_framework.routers import SimpleRouter
+from .views import PostViewSet, UserViewSet
+
+router = SimpleRouter()
+
+router.register('users', UserViewSet, basename='users')
+router.register('', PostViewSet, basename='posts')
+
+urlpatterns = router.urls
